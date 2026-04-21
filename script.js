390f038b7094d99d12a369f244ba1d6b2427a697
Sohaib13g <rehmansohaib73@gmail.com>
2026-04-21 20:12:50 +0500

    Add function to open WhatsApp link

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+function openLink() {
+    window.open("https://whatsapp.com/channel/0029Va9BsJiCsU9MTWDfi03j", "_blank");
+}
 functionGodzillatrading() {
     alert("Welcome Sohaib 🚀 Start your trading journey now!");
 }
