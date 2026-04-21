a1aa0188809cbb36b44f726360a8b9b71355c2ba
Sohaib13g <rehmansohaib73@gmail.com>
2026-04-21 20:18:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-function openLink() {
+function openWhatsApp() {
     window.open("https://whatsapp.com/channel/0029Va9BsJiCsU9MTWDfi03j", "_blank");
-}
-functionGodzillatrading() {
-    alert("Welcome Sohaib 🚀 Start your trading journey now!");
-}
-
-function cardClick(coin) {
-    alert(https://whatsapp.com/channel/0029Va9BsJiCsU9MTWDfi03j!");
 }
